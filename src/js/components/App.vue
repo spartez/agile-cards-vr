405d3ad0fe63021dc17fd53d783e7b1cf2181c52
System <none>
2017-09-14 13:14:54 +0000

    Checkpoint

---
+++ src/js/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {};
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    font-size: 22px;
+    color: red;
+  }
+</style>
