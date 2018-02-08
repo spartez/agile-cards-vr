642cf9d4a7c53e35ae289d3f5c9157c644ce9e5c
Michał Nykiel <michal.nykiel@spartez.com>
2018-02-08 12:27:26 +0100

    server side request to jira

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const axios = require('axios');
 
 app.use(express.static('public'));
 app.use(express.static('dist'));
@@ -9,11 +10,17 @@ app.get('/', function(request, response) {
     response.sendFile(__dirname + '/dist/index.html');
 });
 
-app.get('/board', (request, response) => {
-
+app.get('/board', async (request, response) => {
+    const { data } = await axios.get(`https://00000000.atlassian.net/rest/agile/1.0/board/52/configuration`, {
+        auth: {
+            username: 'test',
+            password: 'Admin123'
+        }
+    });
+    response.send(data);
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(process.env.PORT, function() {
     console.log('Your app is listening on port ' + listener.address().port);
 });
