8519bcd15e4217759d54a3a8e52f0489f68adeff
Victor Debone <victor.debone@spartez.com>
2018-02-08 19:33:02 +0100

    CARD

--- src/js/components/World.vue
+++ src/js/components/World.vue
@@ -1,19 +1,33 @@
 <template>
     <a-scene>
         <a-assets>
+            <a-asset-item id="optimerBoldFont" src="src/fonts/helvetiker_regular.typeface.json"></a-asset-item>
             <img id="bg" src="../../img/360-2.jpg">
+            <img id="task" src="../../img/task.svg">
+            <img id="story" src="../../img/story.svg">
+            <img id="bug" src="../../img/bug.svg">
+            <img id="defaultAvatar" src="../../img/default-avatar.png">
         </a-assets>
-        <board></board>
-        <!-- <a-sky  src="#bg" color="#ECECEC"></a-sky> -->
+        <a-entity position="10 0 0">
+            <board> </board>
+            <a-entity rotation="0 180 0">
+                <a-camera look-controls>
+                    <card></card>
+                </a-camera>
+            </a-entity>
+            <!-- <a-sky  src="#bg" color="#ECECEC"></a-sky> -->
+        </a-entity>
         <a-entity environment="preset: forest; numDressing: 500"></a-entity>
+        <a-entity light="type: ambient; color: #FFF"></a-entity>
     </a-scene>
 </template>
 
 <script>
 import Board from './Board.vue';
+import Card from './Card.vue';
 
     export default {
-        components: { Board },
+        components: { Board, Card },
 
         data() {
             return {
